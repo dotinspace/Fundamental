723c131636ba80eb93f1be1af2948377cda81583
David Peterson <31854491+djpeterso23662@users.noreply.github.com>
2017-11-26 14:35:45 -0500

    Fix VCS2 disappears
    Fix for Issue #2 VCS2 disappears when saved patch is loaded. Previous patches will get VCS not found message.

--- src/MrLumps.cpp
+++ src/MrLumps.cpp
@@ -9,6 +9,6 @@ void init(rack::Plugin *p) {
 	//plugin->name = "MrLumps";
 	plugin->website = "https://github.com/MrLumps";
 	p->addModel(createModel<SEQEuclidWidget>(plugin->slug, "SEQE", "SEQ-Euclid",SEQUENCER_TAG));
-   p->addModel(createModel<VCS1x8Widget>(plugin->slug, "VCS", "1x8 Voltage Controlled Switch",SWITCH_TAG));
-   p->addModel(createModel<VCS2x4Widget>(plugin->slug, "VCS", "2x4 Voltage Controlled Switch",SWITCH_TAG));
+   p->addModel(createModel<VCS1x8Widget>(plugin->slug, "VCS1", "1x8 Voltage Controlled Switch",SWITCH_TAG));
+   p->addModel(createModel<VCS2x4Widget>(plugin->slug, "VCS2", "2x4 Voltage Controlled Switch",SWITCH_TAG));
 }
